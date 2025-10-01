486ebe6bd9f341dc11a591c2b5e9ce5483b16a23
TSAFACK NGUETSOP RYAN TRESOR <ryantsafack22@gmail.com>
2025-10-01 18:36:56 +0100

    Update listetrie.c

--- listetrie.c
+++ listetrie.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 typedef struct cellule {
     int valeur;
@@ -13,6 +12,7 @@ Liste creer_liste_vide();
 Liste inserer_trie(Liste liste, int nouvelle_valeur);
 void afficher_liste(Liste liste);
 void liberer_liste(Liste liste);
+void saisir_elements(Liste *liste_ptr, int nombre);
 
 Liste creer_liste_vide() {
     return NULL;
@@ -69,24 +69,86 @@ void liberer_liste(Liste liste) {
     }
 }
 
+void saisir_elements(Liste *liste_ptr, int nombre) {
+    int i, val;
+
+    if (nombre <= 0) {
+        printf("Aucun élément à insérer.\n");
+        return;
+    }
+
+    printf("\n--- Saisie de %d élément(s) ---\n", nombre);
+    for (i = 0; i < nombre; i++) {
+        printf("Entrez le nombre %d/%d : ", i + 1, nombre);
+        if (scanf("%d", &val) != 1) {
+            printf("Saisie invalide. Opération annulée pour cette série.\n");
+            // Nettoyage du buffer en cas d'erreur de saisie
+            while(getchar() != '\n');
+            return;
+        }
+        *liste_ptr = inserer_trie(*liste_ptr, val);
+    }
+    printf("Saisie terminée.\n");
+}
+
 int main() {
     Liste ma_liste = creer_liste_vide();
-    int val;
+    int nb_elements;
+    int choix;
     
     printf("--- Gestion de Liste Triée Dynamique ---\n");
-    printf("Entrez des nombres entiers, un par un.\n");
-
-     while (printf("Entrez un nombre : ") && scanf("%d", &val) == 1) {
-        ma_liste = inserer_trie(ma_liste, val);
-        afficher_liste(ma_liste);
+    printf("Combien d'éléments voulez-vous insérer initialement ? : ");
+    if (scanf("%d", &nb_elements) == 1) {
+        while(getchar() != '\n'); 
+        saisir_elements(&ma_liste, nb_elements);
+    } else {
+        printf("Saisie du nombre d'éléments invalide. Démarrage avec une liste vide.\n");
+        while(getchar() != '\n'); 
     }
-    printf("Pour arrêter, entrez un caractère non numérique (ex: 'q').\n");
-    printf("------------------------------------------\n");
+    
+    do {
+        printf("\n------------------------------------------\n");
+        printf("Liste actuelle :\n");
+        afficher_liste(ma_liste);
+        printf("\nChoisissez une option :\n");
+        printf("1. Afficher la liste (Liste triée)\n");
+        printf("2. Ajouter de nouveaux éléments (Remplir à nouveau)\n");
+        printf("3. Quitter le programme\n");
+        printf("Votre choix : ");
 
-   
-    while (getchar() != '\n'); 
+        if (scanf("%d", &choix) != 1) {
+            printf("Choix invalide. Veuillez entrer 1, 2 ou 3.\n");
 
-    afficher_liste(ma_liste);
+            while(getchar() != '\n'); 
+            choix = 0; 
+            continue;
+        }
+        while(getchar() != '\n'); 
+
+        switch (choix) {
+            case 1:
+                printf("\n--- Affichage de la Liste ---\n");
+                afficher_liste(ma_liste);
+                break;
+            case 2:
+                printf("\nCombien de nouveaux éléments voulez-vous ajouter ? : ");
+                if (scanf("%d", &nb_elements) == 1) {
+                    while(getchar() != '\n'); 
+                    saisir_elements(&ma_liste, nb_elements);
+                } else {
+                    printf("Saisie du nombre d'éléments invalide.\n");
+                    while(getchar() != '\n'); 
+                }
+                break;
+            case 3:
+                printf("\nFermeture du programme.\n");
+                break;
+            default:
+                printf("\nOption non reconnue. Veuillez choisir 1, 2 ou 3.\n");
+                break;
+        }
+
+    } while (choix != 3);
 
     liberer_liste(ma_liste);
 
