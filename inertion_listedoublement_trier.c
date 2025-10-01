c6173de9f12829e52b56e75bb5b3dae669c91088
KINGUI DJOMO AUDREY <audreydjomo426@gmail.com>
2025-10-01 18:14:25 +0100

    Update inertion_listedoublement_trier.c

--- inertion_listedoublement_trier.c
+++ inertion_listedoublement_trier.c
@@ -7,34 +7,39 @@ typedef struct Node {
     struct Node* prev;
 } Node;
 
+// Création d'un nouveau nœud
 Node* createNode(int data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
-    newNode->next = newNode->prev = NULL;
+    newNode->next = NULL;
+    newNode->prev = NULL;
     return newNode;
 }
 
+// Insertion triée
 void insertSorted(Node** head, int data) {
     Node* newNode = createNode(data);
-    if (*head == NULL) {
+
+    if (*head == NULL) { // Liste vide
         *head = newNode;
         return;
     }
+
     Node* current = *head;
     while (current != NULL && current->data < data) {
         current = current->next;
     }
-    if (current == *head) { // insertion en tête
+
+    if (current == *head) { // Insertion en tête
         newNode->next = *head;
         (*head)->prev = newNode;
         *head = newNode;
-    } else if (current == NULL) { // insertion en queue
+    } else if (current == NULL) { // Insertion en queue
         Node* tail = *head;
-        while (tail->next != NULL) 
-            tail = tail->next;
+        while (tail->next != NULL) tail = tail->next;
         tail->next = newNode;
         newNode->prev = tail;
-    } else { // insertion au milieu
+    } else { // Insertion au milieu
         newNode->next = current;
         newNode->prev = current->prev;
         current->prev->next = newNode;
@@ -42,37 +47,43 @@ void insertSorted(Node** head, int data) {
     }
 }
 
+// Affichage avec flèches pour montrer la double liaison
 void printList(Node* head) {
-    Node* tail = NULL;
-    printf("Liste avant reverse: ");
-    while (head != NULL) {
-        printf("%d <-> ", head->data);
-        tail = head;
-        head = head->next;
+    Node* temp = head;
+    while (temp != NULL) {
+        printf("%d", temp->data);
+        if (temp->next != NULL)
+            printf(" <-> ");
+        temp = temp->next;
     }
-    printf("NULL\n");
-
-    printf("Liste en reverse: ");
-    while (tail != NULL) {
-        printf("%d <-> ", tail->data);
-        tail = tail->prev;
-    }
-    printf("NULL\n");
+    printf("\n");
 }
 
 int main() {
     Node* head = NULL;
-    int n, val;
 
-    printf("Combien d'elements voulez-vous entrer ? ");
-    scanf("%d", &n);
-
-    for (int i = 0; i < n; i++) {
-        printf("Entrez l'element %d: ", i + 1);
-        scanf("%d", &val);
-        insertSorted(&head, val);
+    // Liste initiale
+    int initialElements[] = {3, 7, 10, 15};
+    int size = sizeof(initialElements) / sizeof(initialElements[0]);
+    for (int i = 0; i < size; i++) {
+        insertSorted(&head, initialElements[i]);
     }
 
+    printf("Liste initiale: ");
     printList(head);
+
+    int value;
+    char choix;
+    do {
+        printf("Entrez un element a inserer: ");
+        scanf("%d", &value);
+        insertSorted(&head, value);
+        printf("Liste apres insertion: ");
+        printList(head);
+
+        printf("Voulez-vous inserer un autre element ? (o/n) : ");
+        scanf(" %c", &choix);
+    } while (choix == 'o' || choix == 'O');
+
     return 0;
 }
